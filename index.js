d0bc99185bf8ae69e92d0c67d1df063f0dbca15a
Lisa <lisamao2007@gmail.com>
2026-01-30 19:38:58 +0100

    ...

--- index.js
+++ index.js
@@ -8,75 +8,67 @@ const PORT = 8080
 
 //parsers to make data readable
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 
 //CORS
-app.use(function (req,res,next){
+app.use(function (req, res, next) {
     const allowedOrigins = [
         'http://localhost:8080',
         'http://145.24.237.144:8080',
         'http://145.24.237.144:3000'
     ]
 
-    const origin = req.headers.origin
+    const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin)
-        res.header("Vary", "Origin")
-        res.header("Access-Control-Allow-Credentials", "true")
-    } else if(origin) {
-        res.header("Access-Control-Allow-Origin", origin)
-        res.header("Vary", "Origin")
+        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Vary", "Origin");
+        res.header("Access-Control-Allow-Credentials", "true");
     } else {
-        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Origin", "*");
     }
 
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
-    const pathParts = req.path.split("/").filter(part => part !== "")
+    const cleanPath = req.path.replace(/\/$/, "");
+    const pathParts = cleanPath.split("/").filter(part => part !== "");
 
-    //decides if were on /flowers(collection) or /flowers/123(isItem)
-    const isCollection = pathParts.length <= 1
-    const isItem = pathParts.length > 1
+    let allowedMethods = "OPTIONS";
 
-    let allowedMethods = "OPTIONS"
-
-    if (isCollection){
-        allowedMethods = "GET, POST, OPTIONS"
-    } else if (isItem) {
-        allowedMethods = "GET, PUT, DELETE, OPTIONS"
+    if (pathParts.length === 1 && pathParts[0] === "flowers") {
+        allowedMethods = "GET, POST, OPTIONS";
+    } else if (pathParts.length === 2 && pathParts[0] === "flowers") {
+        allowedMethods = "GET, PUT, DELETE, OPTIONS";
+    } else {
+        allowedMethods = "GET, OPTIONS";
     }
 
-    res.header("Allow", allowedMethods)
+    res.header("Allow", allowedMethods);
 
     if (req.method === "OPTIONS") {
-        res.header("Access-Control-Max-Age", "86400")
-        return res.sendStatus(204)
+        res.header("Access-Control-Max-Age", "86400");
+        return res.sendStatus(204);
     }
 
     if (!allowedMethods.includes(req.method)) {
         return res.status(405).json({
-            error: `method: ${req.method} is not allowed on this path`,
+            error: `Method ${req.method} is not allowed on ${req.path}`,
             allowed: allowedMethods
-        })
+        });
     }
     next()
 })
 
 //middleware that checks if the request method is through json or not
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
     console.log("Requested /")
     console.log(`Request METHOD: ${req.method}`)
 
-    if (req.headers.accept && !req.accepts('json') && !req.accepts('*/*')) {
-        return res.status(406).json({
-            error: "JSON ONLY."
-        });
+    if (req.headers.accept && !req.accepts(['json', '*/*'])) {
+        return res.status(406).json({error: "Method not acceptable Only JSON!!"});
     }
-
-
     next()
-    })
+})
 
 connectDB().then(r =>
     app.listen(PORT, () => {
